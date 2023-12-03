e14cf6db7c0567845138044ad3a6e66230a4c70b
Nizigama jean-davy <jedavy.n@gmail.com>
2023-12-03 14:52:06 +0200

    Refactored integration tests to run them once for all the database suite specs

--- business/databases/backup_test.go
+++ business/databases/backup_test.go
@@ -1,20 +1,12 @@
 package databases_test
 
 import (
-	"context"
-	"database/sql"
-	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/nizigama/ovrsight/business/databases"
 	backupDB "github.com/nizigama/ovrsight/foundation/databases"
 	"github.com/nizigama/ovrsight/foundation/storage"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"github.com/qustavo/dotsql"
-	"github.com/testcontainers/testcontainers-go"
-	"github.com/testcontainers/testcontainers-go/modules/mysql"
-	"github.com/testcontainers/testcontainers-go/wait"
-	"os"
 )
 
 var _ = Describe("Backup", func() {
@@ -24,9 +16,6 @@ var _ = Describe("Backup", func() {
 	var dpbxDvr *storage.DropboxDriverMock
 	var gglDvr *storage.GoogleDriveDriverMock
 	var mysqlDumper *backupDB.MysqlDumperMock
-	var mysqlContainer *mysql.MySQLContainer
-	var err error
-	var ctx context.Context
 
 	BeforeEach(func() {
 
@@ -34,61 +23,6 @@ var _ = Describe("Backup", func() {
 		dpbxDvr = &storage.DropboxDriverMock{}
 		gglDvr = &storage.GoogleDriveDriverMock{}
 		mysqlDumper = &backupDB.MysqlDumperMock{}
-
-		ctx = context.Background()
-
-		mysqlContainer, err = mysql.RunContainer(ctx,
-			testcontainers.WithImage("mysql:latest"),
-			mysql.WithDatabase("test_db"),
-			mysql.WithUsername("user"),
-			mysql.WithPassword("password"),
-			testcontainers.CustomizeRequest(
-				testcontainers.GenericContainerRequest{
-					ContainerRequest: testcontainers.ContainerRequest{
-						ExposedPorts: []string{"3306/tcp"},
-						WaitingFor:   wait.ForLog("socket: '/var/run/mysqld/mysqld.sock'  port: 3306  MySQL Community Server - GPL"),
-					},
-				},
-			),
-		)
-
-		if err != nil {
-			panic(err)
-		}
-
-		p, _ := mysqlContainer.MappedPort(ctx, "3306/tcp")
-		port := p.Int()
-
-		dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", "user", "password", "127.0.0.1", port, "test_db")
-
-		dbConn, err := sql.Open("mysql", dsn)
-		if err != nil {
-			panic(err)
-		}
-
-		dot, err := dotsql.LoadFromFile("../../scripts/queries.sql")
-		if err != nil {
-			panic(err)
-		}
-
-		_, err = dot.WithData(map[string]string{
-			"dbname": "test_db",
-		}).Exec(dbConn, "create-database")
-		if err != nil {
-			panic(err)
-		}
-
-		os.Setenv("DB_HOST", "127.0.0.1")
-		os.Setenv("DB_PORT", fmt.Sprintf("%d", port))
-		os.Setenv("DB_USER", "user")
-		os.Setenv("DB_PASSWORD", "password")
-	})
-
-	AfterEach(func() {
-
-		if err := mysqlContainer.Terminate(ctx); err != nil {
-			panic(err)
-		}
 	})
 
 	Context("with filesystem driver", func() {
@@ -104,7 +38,7 @@ var _ = Describe("Backup", func() {
 				Filename:      "file.sql",
 			}
 
-			err = backer.Backup()
+			err := backer.Backup()
 
 			Expect(err).To(BeNil())
 		})
@@ -114,16 +48,18 @@ var _ = Describe("Backup", func() {
 		It("can backup a database", func() {
 
 			dpbxDvr.On("Upload", []byte{}).Return(nil)
+			mysqlDumper.On("Generate").Return([]byte{}, nil)
 
-			//backer = &databases.Backer{
-			//	StorageDriver:       dpbxDvr,
-			//	DatabaseName: "test_db",
-			//	Filename:     "file.sql",
-			//}
+			backer = &databases.Backer{
+				StorageDriver: dpbxDvr,
+				BackupMethod:  mysqlDumper,
+				DatabaseName:  "test_db",
+				Filename:      "file.sql",
+			}
 
-			//err := backer.Backup()
-			//
-			//Expect(err).To(BeNil())
+			err := backer.Backup()
+
+			Expect(err).To(BeNil())
 		})
 	})
 
@@ -131,16 +67,18 @@ var _ = Describe("Backup", func() {
 		It("can backup a database", func() {
 
 			gglDvr.On("Upload", []byte{}).Return(nil)
+			mysqlDumper.On("Generate").Return([]byte{}, nil)
 
-			//backer = &databases.Backer{
-			//	StorageDriver:       gglDvr,
-			//	DatabaseName: "test_db",
-			//	Filename:     "file.sql",
-			//}
+			backer = &databases.Backer{
+				StorageDriver: gglDvr,
+				BackupMethod:  mysqlDumper,
+				DatabaseName:  "test_db",
+				Filename:      "file.sql",
+			}
 
-			//err := backer.Backup()
-			//
-			//Expect(err).To(BeNil())
+			err := backer.Backup()
+
+			Expect(err).To(BeNil())
 		})
 	})
 })
--- business/databases/databases_suite_test.go
+++ business/databases/databases_suite_test.go
@@ -1,6 +1,14 @@
 package databases_test
 
 import (
+	"context"
+	"database/sql"
+	"fmt"
+	"github.com/qustavo/dotsql"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/modules/mysql"
+	"github.com/testcontainers/testcontainers-go/wait"
+	"os"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -9,5 +17,60 @@ import (
 
 func TestDatabases(t *testing.T) {
 	RegisterFailHandler(Fail)
+
+	ctx := context.Background()
+
+	mysqlContainer, err := mysql.RunContainer(ctx,
+		testcontainers.WithImage("mysql:latest"),
+		mysql.WithDatabase("test_db"),
+		mysql.WithUsername("user"),
+		mysql.WithPassword("password"),
+		testcontainers.CustomizeRequest(
+			testcontainers.GenericContainerRequest{
+				ContainerRequest: testcontainers.ContainerRequest{
+					ExposedPorts: []string{"3306/tcp"},
+					WaitingFor:   wait.ForLog("socket: '/var/run/mysqld/mysqld.sock'  port: 3306  MySQL Community Server - GPL"),
+				},
+			},
+		),
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	p, _ := mysqlContainer.MappedPort(ctx, "3306/tcp")
+	port := p.Int()
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", "user", "password", "127.0.0.1", port, "test_db")
+
+	dbConn, err := sql.Open("mysql", dsn)
+	if err != nil {
+		panic(err)
+	}
+
+	dot, err := dotsql.LoadFromFile("../../scripts/queries.sql")
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = dot.WithData(map[string]string{
+		"dbname": "test_db",
+	}).Exec(dbConn, "create-database")
+	if err != nil {
+		panic(err)
+	}
+
+	os.Setenv("DB_HOST", "127.0.0.1")
+	os.Setenv("DB_PORT", fmt.Sprintf("%d", port))
+	os.Setenv("DB_USER", "user")
+	os.Setenv("DB_PASSWORD", "password")
+
+	defer func() {
+		if err := mysqlContainer.Terminate(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
 	RunSpecs(t, "Databases Suite")
 }
